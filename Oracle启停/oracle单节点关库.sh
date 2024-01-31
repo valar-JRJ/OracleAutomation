5b574085da7668e3a202d7cd5e08d97c2c6550c5
jiangruijie <jiangruijie@picc.com.cn>
2024-01-31 17:59:34 +0800

    增加log记录。从smon进程中找sids用于关库；从关库log中找sids用于开库

--- Oracle启停/oracle单节点关库.sh
+++ Oracle启停/oracle单节点关库.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 # 10.135.66.13
 
-# 切换到oracle用户如果不是
-if [ "$USER" != "oracle" ]; then
-    echo "Switching to oracle user..."
-    su - oracle
-    exit
-fi
-
-source ~/.bash_profile
-
 stop_oracle() {
-
-#  检查数据库状态，关闭数据库
+# 检查数据库状态，关闭数据库
 db_status=$(ps -ef | grep "$ORACLE_SID$" | grep -v grep | grep -i "ora_smon")
 if [ -z "$db_status" ]; then
-    echo "Database $ORACLE_SID already closed."
+    echo "Database $ORACLE_SID already closed."| tee -a $logfile
     return
 else
-    echo "Stopping Oracle database..."
+    echo "Stopping Oracle database..."| tee -a $logfile
     sqlplus / as sysdba <<EOF
     SHUTDOWN IMMEDIATE;
     EXIT;
@@ -29,13 +19,13 @@ while true; do
     log_content=$(tail -n 50 $ORACLE_BASE/diag/rdbms/$ORACLE_SID/$ORACLE_SID/trace/alert_$ORACLE_SID.log)
 
     if echo "$log_content" | grep -q "Instance shutdown complete"; then
-        echo "Oracle Instance shutdown complete"
+        echo "Oracle Instance $ORACLE_SID shutdown complete at $(date)"| tee -a $logfile
         break
     fi
 
     if echo "$log_content" | grep -iq "error"; then
-        echo "error info:"
-        echo "$log_content" | grep -i "error"
+        echo "error info:"| tee -a $logfile
+        echo "$log_content" | grep -i "error"| tee -a $logfile
         exit 1
     fi
 
@@ -49,33 +39,56 @@ while true; do
 
     # if no ora process left
     if [ -z "$process_info" ]; then
-        echo "ORACLE processes all stoped"
+        echo "ORACLE processes all stoped"| tee -a $logfile
         break
     else
-        echo "remained ora process:"
-        echo "$process_info"
-
-        # kill remained ora process
-        read -p "kill remained oracle process (y/n)? " answer
-        case $answer in
+        echo "remained ora process:"| tee -a $logfile
+        echo "$process_info"| tee -a $logfile
+        {
+            # kill remained ora process
+            read -p "kill remained oracle process (y/n)? " answer
+            case $answer in
             [Yy]* ) echo "$process_info" | awk '{print $2}' | xargs kill -9;;
             * ) echo "Exiting program."; exit;;
-        esac
+            esac
+        } 2>&1 | tee -a "$logfile"
     fi
     # sleep 5s
     sleep 5
 done
 }
 
-# 多实例检查
-if [ -n "$ORACLE_SID" ]; then
-    sids=$(grep -oP 'ORACLE_SID=\K[^ ]+' ~/.bash_profile |sort| uniq)
-    for sid in $sids
-    do 
-        export ORACLE_SID=$sid
-        echo "Exported ORACLE_SID: $sid"
-        stop_oracle
-    done
-else
-    echo "ORACLE_SID not set. Please set the ORACLE_SID environment variable."
+# 切换到oracle用户如果不是
+if [ "$USER" != "oracle" ]; then
+    echo "Switching to oracle user..."
+    su - oracle
+    exit
 fi
+
+source ~/.bash_profile
+
+logfile="$HOME/oracle_automation_shutdown_startup.log"
+if [ ! -f "$logfile" ] then
+    touch $logfile
+fi
+
+# 多实例检查
+# 查找所有的ora_smon进程
+processes=$(ps -ef | grep ora_smon)
+
+# 检查processes变量是否为空
+if [ -z "$processes" ]; then
+  echo "No oracle system monitor processes found."| tee -a "$logfile"
+  exit 1
+else
+  # 截取Oracle SID，并将结果存储到sids变量中
+  sids=$(echo "$processes" | awk -F'ora_smon_' '{print $2}'| sort| uniq)
+  echo "oracle sid detected, $sids" | tee -a "$logfile"
+fi
+
+for sid in $sids
+do 
+    export ORACLE_SID=$sid
+    echo "Exported ORACLE_SID for shutdown: $sid"| tee -a "$logfile"
+    stop_oracle
+done
--- Oracle启停/oracle单节点启动.sh
+++ Oracle启停/oracle单节点启动.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 # 10.135.66.13
 
-# 切换到oracle用户如果不是
-if [ "$USER" != "oracle" ]; then
-    echo "Switching to oracle user..."
-    su - oracle
-    exit
-fi
-
-source ~/.bash_profile
-
+# 是否需要考虑启动19c pdb启动
 start_oracle() {
 # 检查数据库状态，启动数据库
 db_status=$(ps -ef | grep "$ORACLE_SID$" | grep -v grep | grep -i "ora_smon")
 if [ -n "$db_status" ]; then
-    echo "Database $ORACLE_SID not closed."
+    echo "Database $ORACLE_SID not closed."| tee -a $logfile
     return
 else
-    echo "Starting Oracle database..."
+    echo "Starting Oracle database..."| tee -a $logfile
     sqlplus / as sysdba <<EOF
     STARTUP;
     EXIT;
@@ -29,31 +21,49 @@ while true; do
     log_content=$(tail -n 50 $ORACLE_BASE/diag/rdbms/$ORACLE_SID/$ORACLE_SID/trace/alert_$ORACLE_SID.log)
 
     if echo "$log_content" | grep -iq "Completed: ALTER DATABASE OPEN"; then
-        echo "ORACLE DATABASE OPENED"
+        echo "ORACLE DATABASE $ORACLE_SID OPENED"| tee -a $logfile
         break
     fi
 
     if echo "$log_content" | grep -iq "error"; then
-        echo "error info"
-        echo "$log_content" | grep -i "error"
+        echo "error info"| tee -a $logfile
+        echo "$log_content" | grep -i "error"| tee -a $logfile
         exit 1
     fi
 
     # sleep 5s
     sleep 5
 done
-
 }
 
-# 多实例检查
-if [ -n "$ORACLE_SID" ]; then
-    sids=$(grep -oP 'ORACLE_SID=\K[^ ]+' ~/.bash_profile |sort| uniq)
-    for sid in $sids
-    do 
-        export ORACLE_SID=$sid
-        echo "Exported ORACLE_SID: $sid"
-        start_oracle
-    done
-else
-    echo "ORACLE_SID not set. Please set the ORACLE_SID environment variable."
+# 切换到oracle用户如果不是
+if [ "$USER" != "oracle" ]; then
+    echo "Switching to oracle user..."
+    su - oracle
+    exit
 fi
+
+source ~/.bash_profile
+
+# 多实例检查
+logfile="$HOME/oracle_automation_shutdown_startup.log"
+
+# 检查日志文件是否存在
+if [ ! -f "$logfile" ]
+then
+    echo "Error: Logfile $logfile not found."
+    # 该情况需考虑从.bash_profile找到所有sids启动
+    exit 1
+fi
+
+# 日志文件中查找Oracle SID，并将结果存储到sids变量中
+sids=$(grep "Exported ORACLE_SID for shutdown:" "$logfile" | awk -F'Exported ORACLE_SID for shutdown: ' '{print $2}'|sort|uniq)
+
+# sids=$(grep -oP 'ORACLE_SID=\K[^ ]+' ~/.bash_profile |sort| uniq)
+
+for sid in $sids
+do 
+    export ORACLE_SID=$sid
+    echo "Exported ORACLE_SID for startup: $sid"| tee -a $logfile
+    start_oracle
+done
